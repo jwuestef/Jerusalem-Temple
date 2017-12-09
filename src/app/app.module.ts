d90a80d42ee3d23fc04dc85b7c9702124cce663c
Jeremy Wuestefeld <jwuestef@gmail.com>
2017-12-09 14:09:25 -0500

    initial setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,50 @@
-import { BrowserModule } from '@angular/platform-browser';
+// Angular Modules
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
 
 
-import { AppComponent } from './app.component';
+// AngularFire
+// import { AngularFireModule, FirebaseAppConfig } from 'angularfire2';
+// import { AngularFireDatabaseModule } from 'angularfire2/database';
+// import { AngularFireAuthModule } from 'angularfire2/auth';
+
+
+// Configuration
+import { AppRoutingModule } from './app-routing.module';
+// import { firebaseConfig } from '../environments/environment';
+import { AppComponent } from './app/app.component';
+
+
+// Components
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+
+
+// Services
+
+
+// 3rd Party Modules & Libraries
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    // Angular Modules
+    BrowserModule,
+    BrowserAnimationsModule,
+    // Firebase
+    // AngularFireModule.initializeApp(firebaseConfig),
+    // AngularFireDatabaseModule,
+    // AngularFireAuthModule,
+    // Routing
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
